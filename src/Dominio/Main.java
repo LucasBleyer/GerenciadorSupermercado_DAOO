95d6ecca67f5eb856b4919b37b54053449f3fb29
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-06-09 20:09:26 -0300

    Revert "add tela de entrar sistema"
    This reverts commit b59ffe6c71e9ee061bd84980b8067e286233a73e.

--- src/Dominio/Main.java
+++ src/Dominio/Main.java
@@ -1,14 +1,20 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
 package Dominio;
 
-import Apresentacao.TelaPrincipalForm;
-
+/**
+ *
+ * @author lucas
+ */
 public class Main {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        
-        TelaPrincipalForm janela = new TelaPrincipalForm();
-        janela.setLocationRelativeTo(null);
-        janela.setVisible(true);
+        // TODO code application logic here
     }
+    
 }
