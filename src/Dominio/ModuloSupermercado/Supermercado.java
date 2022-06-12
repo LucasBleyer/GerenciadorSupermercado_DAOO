9daff6132e7309ae5aed20b2bb495d48cf8b4260
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-06-12 15:29:21 -0300

    classe supermercado

--- src/Dominio/ModuloSupermercado/Supermercado.java
+++ src/Dominio/ModuloSupermercado/Supermercado.java
@@ -1,6 +1,27 @@
 
 package Dominio.ModuloSupermercado;
 
+import Dominio.ModuloPessoa.Administrador;
+import Dominio.ModuloPessoa.Cliente;
+
 public class Supermercado {
     
+    private Cliente cliente;
+    private Administrador administrador;
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Administrador getAdministrador() {
+        return administrador;
+    }
+
+    public void setAdministrador(Administrador administrador) {
+        this.administrador = administrador;
+    }
 }
